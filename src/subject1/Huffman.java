d8ca3534d58b43a1dafd8e1ac323c8298bdfed32
周翔辉 <11603080122@2016.cqut.edu.cn>
2017-11-27 17:12:28 +0800

    gitignore修改idea工作目录

---
+++ src/subject1/Huffman.java
@@ -0,0 +1,12 @@
+package subject1;
+
+/**
+ * Author: Zhou Xianghui
+ * Time: 2017/11/27 17:10
+ * Description:
+ */
+public class Huffman {
+    public static void main(String[] args) {
+
+    }
+}
